02b99006ebd200282d81b2907c9798243d1252ce
Damian Muca <Damian@DamianAndrzejMuca@gmail.com>
2020-03-23 18:55:48 +0100

    Add Love Schabowe restaurant.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import restaurants.chinese.HoanKiem;
 import restaurants.italian.Toscana;
+import restaurants.polish.LoveSchabowe;
 import restaurants.restaurant.Restaurant;
 import simulator.OrdersSimulator;
 
 class Main {
   public static void main(String[] args){
-    Restaurant restaurant = new HoanKiem(5);
+//    Restaurant restaurant = new HoanKiem(5);
 //    Restaurant restaurant = new Toscana(5);
+    Restaurant restaurant = new LoveSchabowe(5);
     restaurant.open();
 
     OrdersSimulator ordersSimulator = new OrdersSimulator(restaurant);
