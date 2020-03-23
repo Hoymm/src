a450c3df9f5968471bf345e93a898154ffbbed3a
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-03-23 19:15:35 +0100

    Change type of workplaces to queue (in order to store only available workplaces).

--- src/restaurants/restaurant/Restaurant.java
+++ src/restaurants/restaurant/Restaurant.java
@@ -5,29 +5,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class Restaurant implements RestaurantClientApi, RestaurantManagersApi {
-  private final List<Workplace> workplaces;
+  private final Queue<Workplace> availableWorkplaces;
   final Queue<Order> ordersToBePrepared; // TODO this should be private
 
   protected Restaurant(int howManyProcessingStations) {
     this.ordersToBePrepared = new ArrayDeque<>();
-    workplaces =
-            Stream.generate(() -> new Workplace(this))
-                  .limit(howManyProcessingStations)
-                  .collect(Collectors.toList());
+    availableWorkplaces =
+        Stream.generate(() -> new Workplace(this))
+            .limit(howManyProcessingStations)
+            .collect(Collectors.toCollection(ArrayDeque::new));
   }
 
   @Override
   public void open() {
-    this.workplaces.forEach(Workplace::open);
+    this.availableWorkplaces.forEach(Workplace::open);
   }
 
   @Override
   public void close() {
-    this.workplaces.forEach(Workplace::close);
+    this.availableWorkplaces.forEach(Workplace::close);
   }
 
   @Override
-  public void makeOrder(List <String> dishes) {
+  public void makeOrder(List<String> dishes) {
     Order newOrder = OrderMaker.createNewOrder(dishes);
     ordersToBePrepared.add(newOrder);
     System.out.printf("New order placed %s\n", newOrder);
