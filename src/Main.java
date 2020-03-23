5a47539db33a563b772fd3d74e5f8e3f0d249301
Damian Muca <Damian@DamianAndrzejMuca@gmail.com>
2020-03-23 18:36:14 +0100

    Add Toscana, Italian restaurant.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import restaurants.chinese.HoanKiem;
+import restaurants.italian.Toscana;
 import restaurants.restaurant.Restaurant;
 import simulator.OrdersSimulator;
 
 class Main {
   public static void main(String[] args){
-    Restaurant restaurant = new HoanKiem();
+    Restaurant restaurant = new HoanKiem(5);
+//    Restaurant restaurant = new Toscana(5);
     restaurant.open();
 
     OrdersSimulator ordersSimulator = new OrdersSimulator(restaurant);
