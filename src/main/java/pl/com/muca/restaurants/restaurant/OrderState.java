9d9e3ee81681925c9a481b1b3afeab5cae67429d
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-05-20 18:53:57 +0200

    Color output info about meal prepare status.

--- src/main/java/pl/com/muca/restaurants/restaurant/OrderState.java
+++ src/main/java/pl/com/muca/restaurants/restaurant/OrderState.java
@@ -1,17 +1,22 @@
 package pl.com.muca.restaurants.restaurant;
 
+import static pl.com.muca.common.Colors.BGREEN;
+import static pl.com.muca.common.Colors.YELLOW;
+import static pl.com.muca.common.Colors.RESET;
+
 public enum OrderState {
-    PLACED("placed"),
-    IS_BEING_PREPARED("being prepared"),
-    READY_TO_PICKUP("ready to pickup");
+  PLACED("placed"),
+  IS_BEING_PREPARED(YELLOW + "being prepared" + RESET),
+  READY_TO_PICKUP(BGREEN + "ready to pickup" + RESET);
 
-    private final String stringName;
-    OrderState(String stringName) {
-        this.stringName = stringName;
-    }
+  private final String stringName;
 
-    @Override
-    public String toString() {
-        return stringName;
-    }
+  OrderState(String stringName) {
+    this.stringName = stringName;
+  }
+
+  @Override
+  public String toString() {
+    return stringName;
+  }
 }
